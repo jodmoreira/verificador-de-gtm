80342b2636405b8ba869674259ecbc12fd190c8a
jodmoreira <31655505+jodmoreira@users.noreply.github.com>
2017-11-28 11:23:14 -0200

    Cria Arquivo CSV Com a Página e o ID do Container
    A partir de uma lista de URLS em TXT o programa acessa cada uma dessas urls procurando por um Script que possua o Termo GTM. Em seguida ele vai e extrai o ID do GTM. Caso não exista ele imprime um aviso na tela e no arquivo de saída.

---
+++ gtm.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+def escreversaida():
+	records.append((tag, linha))
+	dataframe = pd.DataFrame(records)
+	dataframe.to_csv("saida.csv", mode="w", header=["GTM", "Pag"])
+
+# codGTM = raw_input('Escreva o codigo do container do GTM: ')
+
+with open("lista.txt", "r+") as urls:
+	with open("saida.txt", "w+") as saida:
+		x = 0
+		records = []
+		for linha in urls:
+			try:
+				linha = linha.strip()
+				requisicao = requests.get(linha.strip()) #strip verifica se ele realmente tirou a nova linha, caso tenha ficado
+				requisicao = requisicao.text
+				soup = BeautifulSoup(requisicao, "html.parser")
+				gtm = soup.findAll('script')
+				gtm = str(gtm)
+				tag = re.search(r'GTM-\w+', gtm).group(0)
+				x = x + 1
+				print x, "paginas verificadas"
+				print linha.strip()
+				print tag, "\n"
+				escreversaida()
+			except:
+				print "ERRO!!!"
+				print linha, "\n"
+				tag = "Encontrei algum erro"
+				escreversaida()
+	saida.close()
+urls.close()
+print "Terminei"
